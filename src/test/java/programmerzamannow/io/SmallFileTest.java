92d81f2d99b6acd6f781b4f1ea59ee9ceaf9a630
Gilang Chandra Maulana <gilangcm17@gmail.com>
2023-12-02 07:55:28 +0700

    belajar java input output

---
+++ src/test/java/programmerzamannow/io/SmallFileTest.java
@@ -0,0 +1,34 @@
+package programmerzamannow.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SmallFileTest {
+    @Test
+    void writeSmallFile() throws IOException {
+        Path path1 = Path.of("small1.txt");
+        byte[] bytes = "hello world".getBytes();
+        Files.write(path1,bytes);
+        Assertions.assertTrue(Files.exists(path1));
+
+        Path path2 = Path.of("small2.txt");
+        Files.writeString(path2,"hello world");
+        Assertions.assertTrue(Files.exists(path2));
+    }
+
+    @Test
+    void readSmallFiles() throws IOException {
+        Path path1 = Path.of("small1.txt");
+        byte[] bytes = Files.readAllBytes(path1);
+        String content1 = new String(bytes);
+        Assertions.assertEquals("hello world",content1);
+
+        Path path2 = Path.of("small2.txt");
+        String content2 = Files.readString(path2);
+        Assertions.assertEquals("hello world",content2);
+    }
+}
